f66a2f65ebbe72b47afc0ec5391ce3d00a3cccae
jorgeconduto <jorgeconduto@hotmail.com>
2022-08-30 23:34:50 +0100

    3 30 08 2022
    3 30 08 2022

--- src/main/java/pt/jcbox/shoppinglist/shoppinglist/IndexController.java
+++ src/main/java/pt/jcbox/shoppinglist/shoppinglist/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 	@RequestMapping("/")
-	public String index() {
-		return "index";
+	public String login() {
+		return "login";
 	}
 }
